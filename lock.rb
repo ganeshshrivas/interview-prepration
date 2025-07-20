03f1ee7f6591c8f9cc3c3e0a2c96855b1d3b15de
GANESH SHRIVAS <shrivasganesh57@gmail.com>
2025-07-20 11:55:21 +0530

    Create lock.rb

---
+++ lock.rb
@@ -0,0 +1,8 @@
+Optimistic vs Pessimistic locking 
+
+https://medium.com/@radadiyahardik355/understanding-locking-in-rails-optimistic-vs-pessimistic-strategies-a48530e2245e
+
+
+rails 7 improve nested locking witout multiple transaction block
+
+https://www.mintbit.com/blog/simplifying-nested-transactions-in-rails-7-with-with-lock/
